28ec30f9301f1d9431c791a41598bbf17546ac6b
Laetitia <laetitiaschreiner@icloud.com>
2023-02-18 19:49:30 +0100

    Add port to bdd-login

--- application/bdd-connection.php
+++ application/bdd-connection.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 session_start();
 $pdo = new PDO(
-	'mysql:host = ' . $host . ';port = 8889;dbname=' . $dbname . ';charset = utf8',
+	'mysql:host = ' . $host . ';port = ' . $port . ';dbname = ' . $dbname . ';charset = utf8',
 	$user, //identifiant
 	$pass //mot de passe
 );
