237492eadcb4d4d928502499e522721e97a9c2f7
Laetitia <laetitiaschreiner@icloud.com>
2023-02-28 14:12:31 +0100

    Fix issue

--- laetitiaadmin/data.php
+++ laetitiaadmin/data.php
@@ -1,7 +1,7 @@
 
 <?php include '../application/bdd-connection.php'; ?>
 <?php
-$resultSet = $pdo->query('SELECT Article_Id FROM Consulte ORDER BY Article_Id DESC LIMIT 1');
+$resultSet = $pdo->query('SELECT COUNT(Date_consultation) AS consult FROM Consulte ORDER BY Article_Id DESC LIMIT 1');
 $consult = $resultSet->fetch();
 
 $resultSet = $pdo->query('SELECT COUNT(*) AS  cn2 FROM Consulte WHERE Date_Consultation > (NOW() - INTERVAL 7 DAY)');
@@ -10,7 +10,7 @@ $consultation = $resultSet->fetch();
 $resultSet = $pdo->query('SELECT COUNT(*)AS cn1 FROM Article');
 $art = $resultSet->fetch();
 
-$array = [$consult['Article_Id'] . ' vues', $art['cn1'] . ' articles', $consultation['cn2'] . ' consultations ces 7 derniers jours'];
+$array = [$consult['consult'] . ' vues', $art['cn1'] . ' articles', $consultation['cn2'] . ' consultations ces 7 derniers jours'];
 
 header('Content-type: application/json');
 
